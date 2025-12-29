232f253c4beebb7c170ccfff05f1e215ba59092f
Abubakar Sadeeq <abubakarzoomii@gmail.com>
2025-12-29 16:50:17 +0500

    validation fixed

--- client/src/components/AddUserForm.tsx
+++ client/src/components/AddUserForm.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
 import { Box, TextField, Button } from '@mui/material';
-import type { CreateTodoDto, Todo } from '../types/todo.types';
+import type { CreateTodoDto, Todo, UpdateTodoDto } from '../types/todo.types';
 import { toast } from 'react-hot-toast';
 import { useTodo } from '../hooks/useTodos';
+import { Formik } from 'formik';
+import { todoValidationSchema } from '../schema/todoValidationSchema';
 
 interface AddUserFormProps {
   onClose: () => void;
@@ -11,106 +12,119 @@ interface AddUserFormProps {
 
 const AddUserForm = ({ onClose, isEdit }: AddUserFormProps) => {
   const { createTodo, updateTodo } = useTodo();
-  const [formData, setFormData] = useState<CreateTodoDto>({
-    name: '',
-    email: '',
-    age: 0,
-  });
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
 
-    setFormData((prev) => ({
-      ...prev,
-      [name]: name === 'age' ? Number(value) : value,
-    }));
+  const initialValues: CreateTodoDto | UpdateTodoDto = {
+    name: isEdit ? isEdit.name : '',
+    email: isEdit ? isEdit.email : '',
+    age: isEdit ? isEdit.age : 0,
   };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (!formData.name || !formData.email || !formData.age) {
-      toast.error('Please fill all fields');
-      return;
-    }
-
-    try {
-      if (isEdit) {
-        await updateTodo(isEdit.id, formData);
-        toast.success('User Updated');
-      } else {
-        await createTodo(formData);
-        toast.success('User added');
-      }
-    } catch (err) {
-      toast.error(err instanceof Error ? err.message : 'Failed to add user');
-    } finally {
-      onClose();
-      setFormData({ name: '', email: '', age: 0 });
-    }
-  };
-  useEffect(() => {
-    if (isEdit) {
-      setFormData({
-        name: isEdit.name,
-        email: isEdit.email,
-        age: isEdit.age,
-      });
-    }
-  }, [isEdit]);
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      sx={{
-        maxWidth: 400,
-        mx: 'auto',
-        p: 3,
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 2,
-        border: '1px solid #ccc',
-        borderRadius: 2,
+    <Formik
+      initialValues={initialValues}
+      enableReinitialize
+      validationSchema={todoValidationSchema}
+      onSubmit={async (values, { setSubmitting, resetForm }) => {
+        try {
+          const payload: CreateTodoDto = {
+            name: values.name || '',
+            email: values.email || '',
+            age: Number(values.age),
+          };
+
+          if (isEdit) {
+            await updateTodo(isEdit.id, payload);
+            toast.success('User Updated');
+          } else {
+            await createTodo(payload);
+            toast.success('User added');
+          }
+          resetForm();
+          onClose();
+        } catch (err) {
+          toast.error(err instanceof Error ? err.message : 'Failed to submit');
+        } finally {
+          setSubmitting(false);
+        }
       }}
     >
-      <TextField
-        label="Name"
-        name="name"
-        value={formData.name}
-        onChange={handleOnChange}
-        fullWidth
-        sx={{ width: { xs: '100%', sm: 300, md: 400 } }}
-      />
-      <TextField
-        label="Email"
-        type="email"
-        name="email"
-        value={formData.email}
-        onChange={handleOnChange}
-        fullWidth
-      />
-      <TextField
-        label="Age"
-        type="number"
-        name="age"
-        value={formData.age === 0 ? '' : formData.age}
-        onChange={handleOnChange}
-        fullWidth
-        sx={{
-          '& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button':
-            {
-              display: 'none',
-              margin: 0,
-            },
-          '& input[type=number]': {
-            MozAppearance: 'textfield',
-          },
-        }}
-      />
-      <Button type="submit" variant="contained" color="primary">
-        {isEdit ? 'Update User' : 'Add User'}
-      </Button>
-    </Box>
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{
+            maxWidth: 400,
+            mx: 'auto',
+            p: 3,
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            border: '1px solid #ccc',
+            borderRadius: 2,
+          }}
+        >
+          <TextField
+            label="Name"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.name && Boolean(errors.name)}
+            helperText={touched.name && errors.name ? errors.name : ''}
+            fullWidth
+            sx={{ width: { xs: '100%', sm: 300, md: 400 } }}
+          />
+          <TextField
+            label="Email"
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.email && Boolean(errors.email)}
+            helperText={touched.email && errors.email ? errors.email : ''}
+            fullWidth
+          />
+          <TextField
+            label="Age"
+            type="number"
+            name="age"
+            value={values.age === 0 ? '' : values.age}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.age && Boolean(errors.age)}
+            helperText={touched.age && errors.age ? errors.age : ''}
+            fullWidth
+            sx={{
+              '& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button':
+                {
+                  display: 'none',
+                  margin: 0,
+                },
+              '& input[type=number]': {
+                MozAppearance: 'textfield',
+              },
+            }}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={isSubmitting}
+          >
+            {isEdit ? 'Update User' : 'Add User'}
+          </Button>
+        </Box>
+      )}
+    </Formik>
   );
 };
 
